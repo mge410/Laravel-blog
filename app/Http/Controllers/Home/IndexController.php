7a1b08cc698f83048eae8e489db279fce0d83ed7
Vladimir <v0v.voron2005@yandex.ru>
2023-05-06 22:25:01 +0300

    Update seed for project

--- app/Http/Controllers/Home/IndexController.php
+++ app/Http/Controllers/Home/IndexController.php
@@ -14,8 +14,8 @@ class IndexController extends Controller
         $categories = Category::withCount('posts')->take(7)->get()->sortByDesc(function ($category){
             return $category->posts->count();
         });
-        $randomPost = Post::get()->random(6);
-        $mainPostsList = Post::withCount('likes')->withCount('comments')->get();
+        $randomPost = Post::get()->random(Post::all()->count() < 6 ? Post::all()->count() : 6);
+        $mainPostsList = Post::withCount('likes')->withCount('comments')->where('is_main', '=', true)->get();
         return view('home.index', compact('categories', 'randomPost', 'mainPostsList'));
     }
 }
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Role;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
@@ -20,7 +21,7 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
-            'role_id' => 2,
+            'role_id' => Role::query()->where('title', '=', 'user')->get()->pluck('id')->first(),
         ];
     }
 
