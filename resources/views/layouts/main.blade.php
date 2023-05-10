ba82c8dae2c1c8f9f1a4f686fa449a2bf0462b72
Vladimir <v0v.voron2005@yandex.ru>
2023-05-10 16:41:47 +0300

    Update templates sidebar and footer

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -60,7 +60,7 @@
 <footer class="footer-area section-padding">
     <div class="container">
         <div class="row">
-            <div class="col-lg-3  col-md-6 col-sm-6">
+            <div class="col-lg-4  col-md-6 col-sm-6">
                 <div class="single-footer-widget">
                     <h6>About Us</h6>
                     <p>
@@ -69,36 +69,7 @@
                     </p>
                 </div>
             </div>
-            <div class="col-lg-4  col-md-6 col-sm-6">
-                <div class="single-footer-widget">
-                    <h6>Newsletter</h6>
-                    <p>Stay update with our latest</p>
-                    <div class="" id="mc_embed_signup">
-
-                        <form target="_blank" novalidate="true" action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01"
-                              method="get" class="form-inline">
-
-                            <div class="d-flex flex-row">
-
-                                <input class="form-control" name="EMAIL" placeholder="Enter Email" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter Email '"
-                                       required="" type="email">
-
-
-                                <button class="click-btn btn btn-default"><span class="lnr lnr-arrow-right"></span></button>
-                                <div style="position: absolute; left: -5000px;">
-                                    <input name="b_36c4fd991d266f23781ded980_aefe40901a" tabindex="-1" value="" type="text">
-                                </div>
-
-                                <!-- <div class="col-lg-4 col-md-4">
-                                      <button class="bb-btn btn"><span class="lnr lnr-arrow-right"></span></button>
-                                    </div>  -->
-                            </div>
-                            <div class="info"></div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3  col-md-6 col-sm-6">
+            <div class="col-lg-5  col-md-6 col-sm-6">
                 <div class="single-footer-widget mail-chimp">
                     <h6 class="mb-20">Instragram Feed</h6>
                     <ul class="instafeed d-flex flex-wrap">
@@ -113,7 +84,7 @@
                     </ul>
                 </div>
             </div>
-            <div class="col-lg-2 col-md-6 col-sm-6">
+            <div class="col-lg-3 col-md-6 col-sm-6">
                 <div class="single-footer-widget">
                     <h6>Follow Us</h6>
                     <p>Let us be social</p>
