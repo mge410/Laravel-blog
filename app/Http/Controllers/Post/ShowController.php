a3dbfa26353c2b61dcc18cc9fc359fc2a377156f
Vladimir <v0v.voron2005@yandex.ru>
2023-05-24 22:08:48 +0300

    Update views for comments

--- app/Http/Controllers/Post/ShowController.php
+++ app/Http/Controllers/Post/ShowController.php
@@ -4,15 +4,29 @@ namespace App\Http\Controllers\Post;
 
 use App\Http\Controllers\Admin\Post\BaseController;
 use App\Models\Category;
+use App\Models\Comment;
 use App\Models\Post;
+use Carbon\Carbon;
 
 class ShowController extends BaseController
 {
     public function __invoke(Post $post)
     {
-        $categories = Category::withCount('posts')->take(Category::all()->count() < 7 ? Post::all()->count() : 7)->get()->sortByDesc(function ($category) {
-            return $category->posts->count();
-        });
-        return view('post.show', compact('post', 'categories'));
+        $categoryCount = Category::count();
+
+        $categories = Category::query()->select('title')
+            ->withCount('posts')
+            ->limit(min($categoryCount, 7))
+            ->orderByDesc('posts_count')
+            ->get();
+        $comments = Comment::select('id', 'comment', 'created_at', 'post_id', 'user_id')
+            ->where('post_id', '=', $post->id)
+            ->with(['user' => function ($query) {
+                $query->select('id', 'name');
+            }])->get();
+        return view(
+            'post.show',
+            compact('post', 'categories', 'comments')
+        );
     }
 }
