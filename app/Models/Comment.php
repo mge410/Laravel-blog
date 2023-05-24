671c0421179bd2cf5a396bd9975d39a2560ae1b3
Vladimir <v0v.voron2005@yandex.ru>
2023-05-24 21:58:16 +0300

    Add method for comment model relations user

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -10,4 +10,10 @@ class Comment extends Model
     use HasFactory;
     protected $table = 'comments';
     protected $guarded = false;
+
+    public function user()
+    {
+        return $this
+            ->belongsTo(User::class, 'user_id', 'id');
+    }
 }
