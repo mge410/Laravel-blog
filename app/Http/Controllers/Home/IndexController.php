9d85e1d93d733a8109665ea64137ee1e10864f33
Vladimir <v0v.voron2005@yandex.ru>
2023-05-25 22:02:14 +0300

    Add popularPosts and popularTags for sidebar

--- app/Http/Controllers/Home/IndexController.php
+++ app/Http/Controllers/Home/IndexController.php
@@ -6,24 +6,29 @@ use App\Http\Controllers\Controller;
 use App\Models\Category;
 use App\Models\Post;
 
+use App\Models\Tag;
 use function view;
 
 class IndexController extends Controller
 {
     public function __invoke()
     {
-        $postCount = Post::count();
-        $categoryCount = Category::count();
-
         $categories = Category::query()->select('title')
             ->withCount('posts')
-            ->limit(min($categoryCount, 7))
+            ->limit(7)
             ->orderByDesc('posts_count')
             ->get();
 
+        $popularPosts = Post::withCount('likes')
+            ->orderBy('likes_count', 'desc')
+            ->limit(3)
+            ->get();
+        $popularTags = Tag::withCount('posts')
+            ->orderBy('posts_count', 'desc')->limit(10)->get('id', 'title');
+
         $randomPosts = Post::where('is_main', false)
             ->inRandomOrder()
-            ->limit(min($postCount, 6))
+            ->limit(6)
             ->get(['id', 'title', 'preview_image', 'content']);
 
         $mainPosts = Post::with(['tags' => function ($query) {
@@ -39,7 +44,9 @@ class IndexController extends Controller
             compact(
                 'categories',
                 'randomPosts',
-                'mainPosts'
+                'mainPosts',
+                'popularPosts',
+                'popularTags',
             )
         );
     }
--- app/Http/Controllers/Post/ShowController.php
+++ app/Http/Controllers/Post/ShowController.php
@@ -6,27 +6,44 @@ use App\Http\Controllers\Admin\Post\BaseController;
 use App\Models\Category;
 use App\Models\Comment;
 use App\Models\Post;
-use Carbon\Carbon;
+use App\Models\Tag;
 
 class ShowController extends BaseController
 {
     public function __invoke(Post $post)
     {
-        $categoryCount = Category::count();
-
         $categories = Category::query()->select('title')
             ->withCount('posts')
-            ->limit(min($categoryCount, 7))
+            ->limit(7)
             ->orderByDesc('posts_count')
             ->get();
-        $comments = Comment::select('id', 'comment', 'created_at', 'post_id', 'user_id')
+        $popularPosts = Post::withCount('likes')
+            ->orderBy('likes_count', 'desc')
+            ->limit(3)
+            ->get();
+        $comments = Comment::select(
+            'id',
+            'comment',
+            'created_at',
+            'post_id',
+            'user_id'
+        )
             ->where('post_id', '=', $post->id)
             ->with(['user' => function ($query) {
                 $query->select('id', 'name');
             }])->get();
+        $popularTags = Tag::withCount('posts')
+            ->orderBy('posts_count', 'desc')->limit(10)->get('id', 'title');
+
         return view(
             'post.show',
-            compact('post', 'categories', 'comments')
+            compact(
+                'post',
+                'categories',
+                'comments',
+                'popularPosts',
+                'popularTags'
+            )
         );
     }
 }
