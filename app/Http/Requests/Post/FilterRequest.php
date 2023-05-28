01f9c26fb810564d62830f0f1b8278aa8b584850
Vladimir <v0v.voron2005@yandex.ru>
2023-05-28 20:26:59 +0300

    Add filter for posts page

---
+++ app/Http/Requests/Post/FilterRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests\Post;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class FilterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title' => 'nullable|string',
+            'content' => 'nullable|string',
+            'category_id' => 'nullable|integer|exists:categories,id',
+            'tag_ids' => 'nullable|array',
+            'tag_ids.*' => 'nullable|integer|exists:tags,id',
+        ];
+    }
+}
